7d4d77ce58ed1518172bb3fc309d3fd4adfc7472
PointerToNextPole <pointertonextpole@gmail.com>
2020-12-18 23:26:42 +0800

    chapter 7 started

---
+++ vue-router-test/src/components/A.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        Component A
+        <button @click="addRoute">AddRoutes</button>
+        <router-link to="/b">to Component B</router-link>
+    </div>
+</template>
+
+<script>
+    import B from "./B";
+
+    export default {
+
+        methods: {
+            addRoute() {
+                this.$router.addRoutes([
+                    {path: '/b', component: B}
+                ])
+            },
+        },
+
+        beforeCreate() {
+            console.log('before create')
+        },
+        created() {
+            console.log('created')
+        },
+        beforeMount() {
+            console.log('before mount')
+        },
+        mounted() {
+            console.log('mounted')
+        },
+        beforeRouteEnter(to, from, next) {
+            // 不能获取组件实例 `this`
+            console.log('beforeRouteEnter', to, from)
+            next()
+        },
+        beforeRouteUpdate(to, from, next) {
+            console.log('beforeRouteUpdate', to, from)
+            next()
+        },
+        beforeDestroy() {
+            console.log('beforeRouterDestroy')
+        },
+        beforeRouteLeave(to, from, next) {
+            console.log('beforeRouteLeave', to, from)
+            next()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue-router-test/src/router.js
+++ vue-router-test/src/router.js
@@ -1,13 +1,46 @@
 import Vue from 'vue'
 import Route from 'vue-router'
 import HelloWorld from './components/HelloWorld'
+import A from './components/A'
+// import B from './components/B'
 
 Vue.use(Route)
 const routes = [
-    { path: '/hello-world', component: HelloWorld }
+    {path: '/hello-world', component: HelloWorld, meta: {title: 'HelloWorld'}},
+    // {path: '/b', component: B},
+    {path: '/a', component: A, meta: {title: 'A'}}
 ]
 const router = new Route({
     routes
+})
+
+console.log('router', router)
+
+//方法二：
+// Vue.mixin({
+//     beforeCreate() {
+//         console.log(this.$router, this.$route)
+//         this.$route.meta.title && (document.title = this.$route.meta.title)
+//         !this.$route.meta.title && ( document.title = this.$route.meta.title)
+//     }
+// })
+
+router.beforeEach((to, from, next) => {
+    console.log('beforeEach', to, from)
+    // 方法一：
+    to.meta.title && (document.title = to.meta.title)
+    !to.meta.title && (document.title = 'default')
+
+    next()
+})
+
+router.beforeResolve((to, from, next) => {
+    console.log('beforeResolve', to, from)
+    next()
+})
+
+router.afterEach((to, from) => {
+    console.log('afterEach', to, from)
 })
 
 export default router
